a4a90105d14657e9bcc7e7bbd42169283148e50f
Christopher Pace <Christopher_pace@ymail.com>
2018-08-14 11:45:44 -0500

    initial Commit

---
+++ pag2.js
@@ -0,0 +1,11 @@
+function pageinfo() {
+    document.write(document.title);
+    document.write(document.location);
+    document.write(document.lastModified);
+
+}document.write(window.innerHeight);
+document.write(",");
+document.writeln(window.innerWidth);
+
+document.writeln(" Page path is " + window.location.href);
+
